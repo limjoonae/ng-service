cd807115e747bc47fba023cf0b261ab330e7f786
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-02-24 10:12:33 +0700

    add index file for reference component to test

---
+++ test/index.ts
@@ -0,0 +1,4 @@
+export * from 'gos-service';
+// export * from '../component/bootstrap-class.service';
+// export * from '../component/check-data.service';
+// export * from '../component/common.service';
