99873bbdf46e588d9aec5a425e1fdd3aa257a259
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-02-28 11:49:02 +0700

    remove ignore file

--- test/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('gos-service'));
-// export * from '../component/bootstrap-class.service';
-// export * from '../component/check-data.service';
-// export * from '../component/common.service'; 
-//# sourceMappingURL=index.js.map
